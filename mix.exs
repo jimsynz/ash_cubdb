86c573d37e5aea74e4d941151a9d440de9600d17
James Harton <james@harton.nz>
2023-09-30 10:30:54 +1300

    docs: add DSL cheat sheets to the sidebar.

--- mix.exs
+++ mix.exs
@@ -38,7 +38,28 @@ defmodule AshCubDB.MixProject do
               type: "Ash.DataLayer"
             }
           ]
-        ]
+        ],
+        extras:
+          ["README.md"]
+          |> Enum.concat(Path.wildcard("documentation/**/*.{md,livemd,cheatmd}"))
+          |> Enum.map(fn
+            "README.md" -> {:"README.md", title: "Read Me", ash_hq?: false}
+            "documentation/" <> _ = path -> {String.to_atom(path), []}
+          end),
+        groups_for_extras:
+          "documentation/*"
+          |> Path.wildcard()
+          |> Enum.map(fn dir ->
+            name =
+              dir
+              |> Path.split()
+              |> List.last()
+              |> String.split(~r/_+/)
+              |> Enum.map_join(" ", &String.capitalize/1)
+
+            files = dir |> Path.join("**.{md,livemd,cheatmd}") |> Path.wildcard()
+            {name, files}
+          end)
       ]
     ]
   end
